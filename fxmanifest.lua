c19190a2ee0060ce86d5f7600d0fac4734e79a84
gubriansky@sernato.sk <gubriansky@sernato.sk>
2021-05-18 14:03:54 +0200

    can spawn as selected character

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,14 +5,13 @@ author 'mufty'
 description 'MRP Character select'
 version '0.0.1'
 
+dependencies {
+    "mrp_core"
+}
+
 client_scripts {
     'NativeUI/NativeUI.lua',
     'config.lua',
     'client/client.lua',
     'client/client.js',
-}
-
-server_scripts {
-    'config.lua',
-    'server/server.lua',
 }
